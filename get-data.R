7ef0188ee20bf7c30ce9e9cf41b5cad66ee4fe41
Kiu <zolfaghk@onid.orst.edu>
2016-01-03 22:39:02 -0800

    update

--- get-data.R
+++ get-data.R
@@ -6,15 +6,17 @@ library(dplyr)
 # 2. from the manually manipulated dataset in csv form.
 
 ### First method, from the xlsx file ###
-data<-read.xlsx2("Estimates_for_All_Counties.xlsx",  sheetIndex=1, startRow=5, colIndex=1:4, stringsAsFactors=FALSE, header=TRUE)
+Places_Data<-read.xlsx2("Estimates_for_All_Counties.xlsx",  sheetIndex=1, startRow=5, colIndex=1:4, stringsAsFactors=FALSE, header=TRUE)
 name_temp<-read.xlsx2("Estimates_for_All_Counties.xlsx",  sheetIndex=1,  endRow=1,  colIndex=1,
-                      stringsAsFactors=FALSE, header=FALSE) # Extracting the Excel file title to name a column
-names(data)[c(1,4)]<- c("Rank",name_temp)
-data[,1]<- as.numeric(data[,1])
-data[,4]<- as.numeric(data[,4])*100 # multiplied by 100 for the sake of % scale
-write.csv(data, file = "Household_Income.csv", row.names = FALSE)
+stringsAsFactors=FALSE, header=FALSE) # Extracting the Excel file title to name a column
+names(Places_Data)[c(1,4)]<- c("Rank",name_temp)
+Places_Data[,1]<- as.numeric(Places_Data[,1])
+Places_Data[,4]<- as.numeric(Places_Data[,4])*100 # multiplied by 100 for the sake of % scale
+write.csv(Places_Data, file = "Household_Income.csv", row.names = FALSE)
 data<- read.csv("Household_Income.csv", header=TRUE, stringsAsFactors=FALSE)
 
 ### Second method, from the csv file ###
-data<- read.csv("Estimates_for_All_Counties.csv", header=TRUE, stringsAsFactors=FALSE)
-data[,4]<- as.numeric(gsub("%","",data[,4])) # Remove the "%" from the response column and make it numeric
+Places_Data<- read.csv("Estimates_Counties.csv", header=TRUE, stringsAsFactors=FALSE)
+Places_Data[,4]<- as.numeric(gsub("%","",Places_Data[,4])) # Remove the "%" from the response column and make it numeric
+write.csv(Places_Data, file = "Household_Income.csv", row.names = FALSE)
+data<- read.csv("Household_Income.csv", header=TRUE, stringsAsFactors=FALSE)
