918e95e63fc69612b890b4cbbadd53d0164e294f
Charlotte Wickham <cwickham@gmail.com>
2016-01-06 13:08:52 -0800

    Make file paths relative to main project directory

--- get-data.R
+++ get-data.R
@@ -6,17 +6,17 @@ library(dplyr)
 # 2. from the manually manipulated dataset in csv form.
 
 ### First method, from the xlsx file ###
-Places_Data<-read.xlsx2("Estimates_for_All_Counties.xlsx",  sheetIndex=1, startRow=5, colIndex=1:4, stringsAsFactors=FALSE, header=TRUE)
-name_temp<-read.xlsx2("Estimates_for_All_Counties.xlsx",  sheetIndex=1,  endRow=1,  colIndex=1,
+Places_Data<-read.xlsx2("data/Estimates_for_All_Counties.xlsx",  sheetIndex=1, startRow=5, colIndex=1:4, stringsAsFactors=FALSE, header=TRUE)
+name_temp<-read.xlsx2("data/Estimates_for_All_Counties.xlsx",  sheetIndex=1,  endRow=1,  colIndex=1,
 stringsAsFactors=FALSE, header=FALSE) # Extracting the Excel file title to name a column
 names(Places_Data)[c(1,4)]<- c("Rank",name_temp)
 Places_Data[,1]<- as.numeric(Places_Data[,1])
 Places_Data[,4]<- as.numeric(Places_Data[,4])*100 # multiplied by 100 for the sake of % scale
-write.csv(Places_Data, file = "Household_Income.csv", row.names = FALSE)
-data<- read.csv("Household_Income.csv", header=TRUE, stringsAsFactors=FALSE)
+write.csv(Places_Data, file = "data/Household_Income.csv", row.names = FALSE)
+data<- read.csv("data/Household_Income.csv", header=TRUE, stringsAsFactors=FALSE)
 
 ### Second method, from the csv file ###
-Places_Data<- read.csv("Estimates_Counties.csv", header=TRUE, stringsAsFactors=FALSE)
+Places_Data<- read.csv("data/Estimates_Counties.csv", header=TRUE, stringsAsFactors=FALSE)
 Places_Data[,4]<- as.numeric(gsub("%","",Places_Data[,4])) # Remove the "%" from the response column and make it numeric
-write.csv(Places_Data, file = "Household_Income.csv", row.names = FALSE)
-data<- read.csv("Household_Income.csv", header=TRUE, stringsAsFactors=FALSE)
+write.csv(Places_Data, file = "data/Household_Income.csv", row.names = FALSE)
+data<- read.csv("data/Household_Income.csv", header=TRUE, stringsAsFactors=FALSE)
